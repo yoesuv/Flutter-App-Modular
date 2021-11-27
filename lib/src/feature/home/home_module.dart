769aacc5b0b8d572ffb9daca2c6bf4dd6bc6b953
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-11-27 14:16:34 +0700

    :pencil: init bloc on route

--- lib/src/feature/home/home_module.dart
+++ lib/src/feature/home/home_module.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_app_modular/src/feature/home/blocs/home_bloc.dart';
+import 'package:flutter_app_modular/src/feature/home/events/home_event.dart';
 import 'package:flutter_app_modular/src/feature/home/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
@@ -8,6 +11,9 @@ class HomeModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-    ChildRoute('/', child: (context, args) => const HomeScreen()),
+    ChildRoute('/', child: (context, args) => BlocProvider(
+      create: (context) => HomeBloc()..add(HomeEventInit()), 
+      child: const HomeScreen(),
+    )),
   ];
 }
--- lib/src/feature/home/screens/home_screen.dart
+++ lib/src/feature/home/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_modular/src/feature/home/blocs/home_bloc.dart';
-import 'package:flutter_app_modular/src/feature/home/events/home_event.dart';
 import 'package:flutter_app_modular/src/feature/home/states/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,20 +11,18 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Homepage')),
-      body: BlocProvider(create: (context) => HomeBloc()..add(HomeEventInit()),
-        child: BlocBuilder<HomeBloc, HomeState>(
-          builder: (context, HomeState state){
-            if (state is HomeStateSuccess) {
-              final dataSize = state.listPlace.length;
-              return Center(
-                child: Text('Home Screen # data size $dataSize'),
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, HomeState state){
+          if (state is HomeStateSuccess) {
+            final dataSize = state.listPlace.length;
+            return Center(
+              child: Text('Home Screen # data size $dataSize'),
             );
-          } ,
-        )
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } ,
       ),
     );
   }
