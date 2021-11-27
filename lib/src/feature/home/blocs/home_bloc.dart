d319745e6e0f4b7103bfeb3fdd5cf988a788219b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-11-27 10:46:49 +0700

    :pencil: setup new bloc

--- lib/src/feature/home/blocs/home_bloc.dart
+++ lib/src/feature/home/blocs/home_bloc.dart
@@ -2,28 +2,22 @@ import 'package:flutter_app_modular/src/feature/home/events/home_event.dart';
 import 'package:flutter_app_modular/src/feature/home/models/place_model.dart';
 import 'package:flutter_app_modular/src/feature/home/repositories/home_repository.dart';
 import 'package:flutter_app_modular/src/feature/home/states/home_state.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
 
   final HomeRepository _homeRepository = HomeRepository();
 
-  HomeBloc(HomeState initialState) : super(initialState);
-
-  @override
-  void onEvent(HomeEvent event) {
-    super.onEvent(event);
-    if (event is HomeEventInit) {
-      _loadPlaces();
-    }
+  HomeBloc() : super(HomeStateInit()) {
+    on<HomeEventInit>(_loadPlaces);
   }
 
-  Stream<HomeState> _loadPlaces() async*{
+  Future<void> _loadPlaces(HomeEventInit event, Emitter<HomeState> emit) async{
     try {
       List<PlaceModel> list = await _homeRepository.getListPlace();
-      yield HomeStateSuccess(list);
+      return emit(HomeStateSuccess(list));
     } catch (e) {
-      yield HomeStateFailed();
+      return emit(HomeStateFailed());
     }
   }
 
--- lib/src/feature/home/screens/home_screen.dart
+++ lib/src/feature/home/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_modular/src/feature/home/blocs/home_bloc.dart';
+import 'package:flutter_app_modular/src/feature/home/events/home_event.dart';
+import 'package:flutter_app_modular/src/feature/home/states/home_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -8,8 +12,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Homepage')),
-      body: const Center(
-        child: Text('This is Initial Page'),
+      body: BlocProvider(create: (context) => HomeBloc()..add(HomeEventInit()),
+        child: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, HomeState state){
+            if (state is HomeStateSuccess) {
+              final dataSize = state.listPlace.length;
+              return Center(
+                child: Text('Home Screen # data size $dataSize'),
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } ,
+        )
       ),
     );
   }
