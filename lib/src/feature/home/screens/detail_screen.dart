fcf97ed5db46a1971c6f1c737a8b918fa2333cc2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-11-28 05:01:46 +0700

    :sparkles: show data detail screen

--- lib/src/feature/home/screens/detail_screen.dart
+++ lib/src/feature/home/screens/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_modular/src/feature/home/models/place_model.dart';
 
@@ -11,8 +12,45 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Detail'), centerTitle: true),
-      body: const Center(
-        child: Text('Detail Screen'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+              width: double.infinity,
+              height: 250.0,
+              child: CachedNetworkImage(
+                imageUrl: placeModel?.gambar ?? '',
+                fit: BoxFit.cover,
+              )
+          ),
+          _textTitle(),
+          _textDescription(),
+        ],
+      ),
+    );
+  }
+
+  Widget _textTitle() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Text(placeModel?.nama ?? '',
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _textDescription() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Text(placeModel?.deskripsi ?? '',
+        style: const TextStyle(
+            color: Colors.black,
+            fontSize: 14.0,
+        ),
       ),
     );
   }
